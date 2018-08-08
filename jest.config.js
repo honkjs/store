c1346d76ffbe1ec6ca84d2f8e5a6f7adecda1b3e
Kellen Piffner <k@8labs.com>
2018-08-08 11:56:15 -0700

    🚚 HONK! Setting up as standalone repo.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
-const config = require('../jest.config.common.json');
-
-module.exports = {
-  ...config,
+const config = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testPathIgnorePatterns: ['/lib/', '/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['**/*.{ts}', '!**/node_modules/**', '!**/*.d.ts'],
+  collectCoverage: true,
+  testEnvironment: 'node',
 };
+
+module.exports = config;
